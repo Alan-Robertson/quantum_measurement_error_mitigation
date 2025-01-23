26b973bcbf9a35152543abce0426d09f72c68452
Alan Robertson <alan.robertson@sydney.edu.au>
2025-01-24 10:18:50 +1100

    Belated commit of geller sun

---
+++ src/PatchedMeasCal/geller_sun.py
@@ -0,0 +1,11 @@
+def calculate_correlators(backend, provider=None):
+    # get n_qubits
+    
+
+def correlator_A(results):
+    for i in results:
+        for j in i:
+            ptrace(results[i]) - ptrace(n_res[j])
+
+def correlator_B(results):
+
